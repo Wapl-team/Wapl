2fe9ab0308eabfbe777d8f998fe6214abfb8663e
yunjeong <stac_cato@naver.com>
2023-01-31 18:05:02 +0900

    feat: comment page 작성 시간 추가

--- server/apps/wapl/models.py
+++ server/apps/wapl/models.py
@@ -1,5 +1,8 @@
 from django.db import models
+
 from datetime import datetime, timedelta
+from django.utils import timezone
+
 
 class Comment(models.Model):
     content = models.TextField(default='')
@@ -10,3 +13,18 @@ class Comment(models.Model):
     # user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comment_user")
     # plan_post=models.ForeignKey(Plan, on_delete=models.CASCADE, related_name="comment_post") 
     
+    @property
+    def created_string(self):
+        time = datetime.now(tz=timezone.utc) - self.created_at
+
+        if time < timedelta(minutes=1):
+            return '방금 전'
+        elif time < timedelta(hours=1):
+            return str(int(time.seconds / 60)) + '분 전'
+        elif time < timedelta(days=1):
+            return str(int(time.seconds / 3600)) + '시간 전'
+        elif time < timedelta(days=7):
+            time = datetime.now(tz=timezone.utc).date() - self.created_at.date()
+            return str(time.days) + '일 전'
+        else:
+            return False

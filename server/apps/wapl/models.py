c3972785060abf7b0c210bf90f8f681607a44bd9
spark2357 <psbted@naver.com>
2023-01-31 14:17:06 +0900

    feat: google login

--- server/apps/wapl/models.py
+++ server/apps/wapl/models.py
@@ -1,2 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
+class User(AbstractUser):
+    first_name = None
+    last_name = None
+    name = models.CharField(max_length=20)
+    nickname = models.CharField(max_length=20)
+    birth = None
+    gender = None
+    job = None
+    desc = None
+    email = models.EmailField(null=True)

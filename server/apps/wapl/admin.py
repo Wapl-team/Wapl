5d8c2102d64b645324db71422c0923363071b1ee
Junseo <monstazo911@gmail.com>
2023-02-04 01:27:51 +0900

    feat:revive private plan

--- server/apps/wapl/admin.py
+++ server/apps/wapl/admin.py
@@ -1,15 +1,9 @@
 from django.contrib import admin
 from . import models
 
-class PublicPlanAdmin(admin.ModelAdmin):
-  model = models.PublicPlan
-  list_display = ['title', 'owner', 'get_meetings']
-  @admin.display(ordering='meeting__plans', description='Meeting')
-  def get_meetings(self, obj):
-    return obj.plan_meetings.all()
-  
   
 admin.site.register(models.Comment)
-admin.site.register(models.PublicPlan, PublicPlanAdmin)
+admin.site.register(models.PublicPlan)
+admin.site.register(models.PrivatePlan)
 admin.site.register(models.User)
 admin.site.register(models.Meeting)

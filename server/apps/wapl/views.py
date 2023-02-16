345fe5051978814920135aef6ef66344bd95f7b4
kimseo18 <kimseoyeong108@gmail.com>
2023-02-16 09:29:31 +0900

    feat: signup/extra_signup 오류 문구 추가

--- server/apps/wapl/views.py
+++ server/apps/wapl/views.py
@@ -471,21 +471,24 @@ def start(request:HttpRequest, *args, **kwargs):
 def signup(request:HttpRequest, *args, **kwargs):
     default_image_index = random.randint(1, 4)
     if request.method == 'POST':
-        form = forms.SignupForm(request.POST, request.FILES)
-        if form.is_valid():
-            user = form.save()
-            image = request.FILES.get("image")
-            profile = Profile(user=user, image=image)
-            profile.save()
-            auth.login(request, user, backend='django.contrib.auth.backends.ModelBackend')
-            return redirect('wapl:main')
-        else:
-            # 오류 메세지 띄워야 함 (이름, 닉네임, 아이디, 비밀번호를 정확히 입력해주세요 같은 내용)
-            context = {
-                'default_src': f'/static/default_image/{default_image_index}.png',
-                "form": form,
-            }
-            return render(request, "signup.html", context=context)
+      form = forms.SignupForm(request.POST, request.FILES)
+
+      if form.is_valid():
+          user = form.save()
+          image = request.FILES.get("image")
+          profile = Profile(user=user, image=image)
+          profile.save()
+          auth.login(request, user, backend='django.contrib.auth.backends.ModelBackend')
+          return redirect('wapl:main')
+      else:
+          # 오류 메세지 띄워야 함 (이름, 닉네임, 아이디, 비밀번호를 정확히 입력해주세요 같은 내용)
+          err_msg="모든 항목을 정확하게 입력해 주세요"
+          context = {
+              'default_src': f'/static/default_image/{default_image_index}.png',
+              "form": form,
+              "err_msg":err_msg
+          }
+      return render(request, "signup.html", context=context)
     else:
         context = {
             'default_src': f'/static/default_image/{default_image_index}.png'
@@ -505,9 +508,11 @@ def extra_signup(request:HttpRequest, *args, **kwargs):
             return redirect('wapl:main')
         else:
             # 정보 틀렸다는 에러 메세지 띄워야 함.
+            err_msg="잘못된 정보입니다. 다시 입력하세요"
             context = {
                 'default_src': f'/static/default_image/{default_image_index}.png',
                 "form": form,
+                "err_msg":err_msg
             }
             return render(request, "extra_signup.html", context=context)
     else:

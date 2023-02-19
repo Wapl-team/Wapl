f0f4f78d3597424add74a0d9440976b49c6ab1ba
kimseo18 <kimseoyeong108@gmail.com>
2023-02-19 22:55:19 +0900

    feat: sidebar open/close 효과 meeting_main에도 적용

--- server/static/javascripts/meeting_main.js
+++ server/static/javascripts/meeting_main.js
@@ -86,14 +86,18 @@ function openToggle() {
   // document.getElementById("sidebar").style.width = "250px";
   // document.getElementById("sidebar").style.paddingTop = "2rem";
   // document.getElementById("sidebar").style.paddingBottom = "2rem";
-  document.getElementById("sidebar").style.display = "block";
+  document.getElementById("sidebar").style.visibility="visible";
+  document.getElementById("sidebar").style.transition="all 0.1s";
+  document.getElementById("sidebar").style.opacity="1";
 }
 
 function closeToggle() {
   // document.getElementById("sidebar").style.width = "0";
   // document.getElementById("sidebar").style.paddingTop = "0";
   // document.getElementById("sidebar").style.paddingBottom = "0";
-  document.getElementById("sidebar").style.display = "none";
+  document.getElementById("sidebar").style.visibility="hidden";
+  document.getElementById("sidebar").style.transition="all 0.1s";
+  document.getElementById("sidebar").style.opacity="0";
 }
 const navBtn = document.querySelector(".nav_bar_button");
 const sidebarMenu = document.querySelector(".sidebar-menu");
